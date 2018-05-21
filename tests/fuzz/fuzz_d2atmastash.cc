993825062bd01b1c502cc93f1cf3b22caba04895
Ryan Liptak <squeek502@hotmail.com>
2018-05-21 04:15:59 -0700

    Normalize indentation/formatting

--- tests/fuzz/fuzz_d2atmastash.cc
+++ tests/fuzz/fuzz_d2atmastash.cc
@@ -1,14 +1,14 @@
 #include "d2itemreader.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-    d2data_use_default(&g_d2data);
-    d2atmastash stash;
-    uint32_t bytesRead;
-    d2err err = d2atmastash_parse(data, size, &stash, &bytesRead);
-    if (err == D2ERR_OK)
-    {
-        d2atmastash_destroy(&stash);
-    }
-    d2data_destroy(&g_d2data);
-    return 0;
+	d2data_use_default(&g_d2data);
+	d2atmastash stash;
+	uint32_t bytesRead;
+	d2err err = d2atmastash_parse(data, size, &stash, &bytesRead);
+	if (err == D2ERR_OK)
+	{
+		d2atmastash_destroy(&stash);
+	}
+	d2data_destroy(&g_d2data);
+	return 0;
 }
--- tests/fuzz/fuzz_d2char.cc
+++ tests/fuzz/fuzz_d2char.cc
@@ -1,16 +1,14 @@
-extern "C" {
 #include "d2itemreader.h"
-}
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-    d2data_use_default(&g_d2data);
-    d2char character;
-    uint32_t bytesRead;
-    d2err err = d2char_parse(data, size, &character, &bytesRead);
-    if (err == D2ERR_OK)
-    {
-        d2char_destroy(&character);
-    }
-    d2data_destroy(&g_d2data);
-    return 0;
+	d2data_use_default(&g_d2data);
+	d2char character;
+	uint32_t bytesRead;
+	d2err err = d2char_parse(data, size, &character, &bytesRead);
+	if (err == D2ERR_OK)
+	{
+		d2char_destroy(&character);
+	}
+	d2data_destroy(&g_d2data);
+	return 0;
 }
