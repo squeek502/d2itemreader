bc0b3051ad9ed6b9d7b8732f12858a4fa58879a3
Ryan Liptak <squeek502@hotmail.com>
2019-02-12 02:48:06 -0800

    Fix copy command in docs/make.bat

--- docs/make.bat
+++ docs/make.bat
@@ -3,7 +3,7 @@
 call npm install https://github.com/squeek502/moxygen
 
 call doxygen
-copy /Y "../README.md" "./INDEX.md"
+copy /Y "..\README.md" ".\INDEX.md"
 call "./node_modules/.bin/moxygen" ./xml --anchors
 call gitbook install
 call gitbook build
