52ad5411444ed02565ef7c8faba8315450396c02
Ryan Liptak <squeek502@hotmail.com>
2019-02-11 16:23:43 -0800

    Remove unnecessary macro in d2err.c

--- src/d2err.c
+++ src/d2err.c
@@ -1,29 +1,24 @@
 #include "d2err.h"
 
-#define D2ERR_MAP(XX) \
-	XX(D2ERR_OK, "no error") \
-	XX(D2ERR_FILE, "failed to open file") \
-	XX(D2ERR_OUT_OF_MEMORY, "out of memory") \
-	XX(D2ERR_PARSE, "unknown parse error") \
-	XX(D2ERR_PARSE_BAD_HEADER_OR_TAG, "bad header or tag") \
-	XX(D2ERR_PARSE_UNEXPECTED_SOCKETED_ITEM, "unexpected socket item (child with no parent)") \
-	XX(D2ERR_PARSE_TRAILING_BYTES, "unparsed bytes at end of file") \
-	XX(D2ERR_PARSE_NOT_ENOUGH_BYTES, "not enough bytes in file") \
-	XX(D2ERR_PARSE_STRING_TOO_LONG, "string data too long") \
-	XX(D2ERR_PARSE_UNEXPECTED_EOF, "unexpected end of file") \
-	XX(D2ERR_PARSE_UNEXPECTED_NONSOCKETED_ITEM, "unexpected non-socket item") \
-	XX(D2ERR_PARSE_TOO_MANY_STASH_PAGES, "impossibly large number of stash pages") \
-	XX(D2ERR_PARSE_TOO_FEW_STASH_PAGES, "actual number of stash pages lower than number given in header") \
-	XX(D2ERR_DATA_NOT_LOADED, "game data (.txt files) not loaded") \
-	XX(D2ERR_UNSUPPORTED_VERSION, "unsupported game version")
-
-#define D2ERR_STR_GEN(name, msg) case name: return msg;
 const char* d2err_str(d2err err)
 {
 	switch (err)
 	{
-		D2ERR_MAP(D2ERR_STR_GEN)
+		case D2ERR_OK: return "no error";
+		case D2ERR_FILE: return "failed to open file";
+		case D2ERR_OUT_OF_MEMORY: return "out of memory";
+		case D2ERR_PARSE: return "unknown parse error";
+		case D2ERR_PARSE_BAD_HEADER_OR_TAG: return "bad header or tag";
+		case D2ERR_PARSE_UNEXPECTED_SOCKETED_ITEM: return "unexpected socket item (child with no parent)";
+		case D2ERR_PARSE_TRAILING_BYTES: return "unparsed bytes at end of file";
+		case D2ERR_PARSE_NOT_ENOUGH_BYTES: return "not enough bytes in file";
+		case D2ERR_PARSE_STRING_TOO_LONG: return "string data too long";
+		case D2ERR_PARSE_UNEXPECTED_EOF: return "unexpected end of file";
+		case D2ERR_PARSE_UNEXPECTED_NONSOCKETED_ITEM: return "unexpected non-socket item";
+		case D2ERR_PARSE_TOO_MANY_STASH_PAGES: return "impossibly large number of stash pages";
+		case D2ERR_PARSE_TOO_FEW_STASH_PAGES: return "actual number of stash pages lower than number given in header";
+		case D2ERR_DATA_NOT_LOADED: return "game data (.txt files) not loaded";
+		case D2ERR_UNSUPPORTED_VERSION: return "unsupported game version";
+		default: return "unknown error";
 	}
-	return "unknown error";
 }
-#undef D2ERR_STR_GEN
